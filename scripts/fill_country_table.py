17510b3631ead62be87d5b0c047b832cd1939e0e
thisi5patrick <patrykbielen97@gmail.com>
2022-06-10 15:08:10 +0200

    Modified fill_country_table script to be more elegant

--- scripts/fill_country_table.py
+++ scripts/fill_country_table.py
@@ -10,15 +10,14 @@ driver.get("https://sustainablesources.com/resources/country-abbreviations/")
 
 cur = con.cursor()
 
-for i in range(3, 249):
-    country_long_name = driver.find_element(
-        By.XPATH,
-        f"/html/body/div/div/div/div[1]/div/article/div/div/div/div[3]/figure/table/tbody/tr[{i}]/td[1]",
-    ).text
-    country_short_name = driver.find_element(
-        By.XPATH,
-        f"/html/body/div/div/div/div[1]/div/article/div/div/div/div[3]/figure/table/tbody/tr[{i}]/td[2]",
-    ).text
+table = driver.find_elements(By.XPATH, "/html/body/div/div/div/div[1]/div/article/div/div/div/div[3]/figure/table/tbody/tr")
+
+for row in table:
+    items = row.find_elements(By.TAG_NAME, "td")
+    if not items:
+        continue
+    country_long_name = items[0].text
+    country_short_name = items[1].text
 
     cur.execute(
         "insert into country(long_name, short_name) values (?, ?)",
