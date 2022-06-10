9f1860357bce2ad96d016614ff769ec2c49e13a5
thisi5patrick <patrykbielen97@gmail.com>
2022-06-10 15:03:13 +0200

    Modified script to be more elegant

--- scripts/fill_aircraft_table.py
+++ scripts/fill_aircraft_table.py
@@ -10,19 +10,14 @@ driver.get("https://en.wikipedia.org/wiki/List_of_aircraft_type_designators")
 
 cur = con.cursor()
 
-for i in range(3, 249):
-    icao = driver.find_element(
-        By.XPATH,
-        f"/html/body/div[3]/div[3]/div[5]/div[1]/table/tbody/tr[{i}]/td[1]",
-    ).text
-    iata = driver.find_element(
-        By.XPATH,
-        f"/html/body/div[3]/div[3]/div[5]/div[1]/table/tbody/tr[{i}]/td[2]",
-    ).text
-    model = driver.find_element(
-        By.XPATH,
-        f"/html/body/div[3]/div[3]/div[5]/div[1]/table/tbody/tr[{i}]/td[3]",
-    ).text
+table = driver.find_elements(By.XPATH, "/html/body/div[3]/div[3]/div[5]/div[1]/table/tbody/tr")
+for row in table:
+    items = row.find_elements(By.TAG_NAME, 'td')
+    if not items:
+        continue
+    icao = items[0].text
+    iata = items[1].text
+    model = items[2].text
 
     cur.execute(
         "insert into aircraft(model, icao, iata) values (?, ?, ?)",
